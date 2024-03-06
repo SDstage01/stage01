c15fd97646b58f2b6489f8df1aa87d7d67428027
2009wc <145115444+2009wc@users.noreply.github.com>
2024-03-06 16:39:38 +0000

    Delete ASE-master directory

--- ASE-master/FlightCodeComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class FlightCodeComparator {
-	
-	public int compare(Flight f1, Flight f2) {
-		return f1.getFlightCode().compareTo(f2.getFlightCode());
-
-	}
-}
